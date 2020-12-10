0c9534839f3c017e179764b9f904659fec39dd45
Atari <jager@djem.ru>
2020-12-10 14:13:50 +0300

    Day 10

--- day10b.js
+++ day10b.js
@@ -5,13 +5,14 @@ const input = data.split(/\r?\n/).map(n => +n).sort((a, b) => a - b);
 const jolts = [0, ...input, input[input.length - 1] + 3];
 
 const permutations = adapters => {
-  const power = 1 << (adapters.length - 2);
+  const mutabor = adapters.slice(1, adapters.length - 1);
+  const power = 1 << mutabor.length;
   const first = adapters[0];
   const last = adapters[adapters.length - 1];
   let variants = 0;
 
   for (let i = 0; i < power; ++i) {
-    const chain = [first, ...adapters.slice(1, adapters.length - 1).filter((v, index) => i & (1 << index)), last];
+    const chain = [first, ...mutabor.filter((v, index) => i & (1 << index)), last];
     variants += chain.reduce((acc, n, i, v) => (i && n - v[i - 1] > 3) ? 0 : acc, 1);
   }
 
