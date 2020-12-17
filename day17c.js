864b030fc9312699e97b33777e2a95eb9abf0b0c
Atari <jager@djem.ru>
2020-12-17 13:54:35 +0300

    Day 17

--- day17c.js
+++ day17c.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const data = fs.readFileSync('input17.txt', { encoding: 'utf8' });
 
 const world = {}, anotherWorld = {}, dimensions = 4, cycles = 6;
-const boundaries = [].concat(Array(dimensions).fill([0, 0]));
 let w = world, aw = anotherWorld; // shout out to chahi
+const fill = (withWhat, what = []) => what.concat(Array(dimensions - what.length).fill(withWhat));
+const boundaries = fill([0, 0]);
 
 const put = (world, coords, v) => {
   for (let dimension = 0; dimension < dimensions; ++dimension) {
@@ -27,20 +28,20 @@ const get = (world, coords, delta = []) =>
 data.split(/\r?\n/g)
   .forEach((row, y) =>
     row.split('').forEach((cell, x) =>
-        cell === '#' && put(world, [x, y].concat(Array(dimensions - 2).fill(0)), 1)));
+        cell === '#' && put(world, fill(0, [x, y]), 1)));
 
 // main recursor that recurses whatever the fuck! =)
-const rec = (dimension, coord, callback, from, to) => {
+const rec = (dimension, coord, callback, boundaries) => {
   if (dimension === dimensions) {
     callback(coord);
-  } else for (let i = from(dimension), l = to(dimension); i <= l; ++i) {
+  } else for (let i = boundaries[dimension][0] - 1, l = boundaries[dimension][1] + 1; i <= l; ++i) {
     coord[dimension] = i;
-    rec(dimension + 1, coord, callback, from, to);
+    rec(dimension + 1, coord, callback, boundaries);
   }
 }
 
 const sumMatrix = []; // that's the multidimensional matrix of neighbour deltas
-rec(0, [], coord => coord.some(n => n) && sumMatrix.push(coord.concat()), _ => -1, _ => 1);
+rec(0, [], coord => coord.some(n => n) && sumMatrix.push(coord.concat()), fill([0, 0]));
 
 for (let cycle = 0; cycle < cycles; ++cycle) {
   rec(0, [], (coord) => {
@@ -50,12 +51,12 @@ for (let cycle = 0; cycle < cycles; ++cycle) {
     } else {
       put(aw, coord, neighbours === 3 ? 1 : 0);
     }
-  }, dimension => boundaries[dimension][0] - 1, dimensions => boundaries[dimensions][1] + 1);
+  }, boundaries);
 
   [w, aw] = [aw, w];
 }
 
 let cubes = 0;
-rec(0, [], coord => cubes += get(w, coord), d => boundaries[d][0], d => boundaries[d][1]);
+rec(0, [], coord => cubes += get(w, coord), boundaries);
 
 console.log(cubes);
